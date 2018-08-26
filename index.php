784afe1e27b2622f65f9f7b42e092562d6f374ff
Flávio Tassan <tassanf@gmail.com>
2018-08-26 17:39:29 -0300

    gitignore configured, some minor changes in index and added favicon

--- index.php
+++ index.php
@@ -4,107 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="images/favicon.ico">
+
     <title>the frisson</title>
     
     <link rel="stylesheet" type="text/css" href="css/petal.css">
     <link rel="stylesheet" type="text/css" href="css/site.css">
 </head>
+
 <body class="background">
     <header>
-        <div class="container">
-                <h1>welcome, to the frisson &#x1F4AB;</h1>
-        </div>
+            <div class="container">
+                    <h1>the frisson &#x1F4AB;</h1>
+            </div>
     </header>
+
     <div class="container">
-
-            
-            <p>Are you feeling sad today? or depressed? anxiety?</p>
-            <p><b>We</b> are here to help you feel <i>better</i> even if it's just a little bit</p>
-            <p><b>How?</b> with music! &#x1F3B6;</p>
-
-            <?php
-                echo "Hi, thanks for visiting from ";
-                echo(ip_info("","Country"));
-            ?>
-            
+        <div class="col-1" style="margin-top: 200px; margin-botton: 200px">
+            <input type="submit" value="I want to hear something" class="btn hollow" >   
+            <input type="submit" value="I want to say something" class="btn hollow" >
+        
+        </div>
     </div>
     
-
-</body>
-<footer>
-    <div class="container mt-200">
+    <div class="footer" >
             <span>Made with &#x2764; by <a href="http://tassan.me" target="_blank">Tassan</a></span>
     </div>
-</footer>
+</body>
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="js/jquery.fs.selecter.min.js"></script>
 </html>
-
-<?php
-
-function ip_info($ip = NULL, $purpose = "location", $deep_detect = TRUE) {
-    $output = NULL;
-    if (filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
-        $ip = $_SERVER["REMOTE_ADDR"];
-        if ($deep_detect) {
-            if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
-                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-            if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
-                $ip = $_SERVER['HTTP_CLIENT_IP'];
-        }
-    }
-    $purpose    = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
-    $support    = array("country", "countrycode", "state", "region", "city", "location", "address");
-    $continents = array(
-        "AF" => "Africa",
-        "AN" => "Antarctica",
-        "AS" => "Asia",
-        "EU" => "Europe",
-        "OC" => "Australia (Oceania)",
-        "NA" => "North America",
-        "SA" => "South America"
-    );
-    if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
-        $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
-        if (@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
-            switch ($purpose) {
-                case "location":
-                    $output = array(
-                        "city"           => @$ipdat->geoplugin_city,
-                        "state"          => @$ipdat->geoplugin_regionName,
-                        "country"        => @$ipdat->geoplugin_countryName,
-                        "country_code"   => @$ipdat->geoplugin_countryCode,
-                        "continent"      => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
-                        "continent_code" => @$ipdat->geoplugin_continentCode
-                    );
-                    break;
-                case "address":
-                    $address = array($ipdat->geoplugin_countryName);
-                    if (@strlen($ipdat->geoplugin_regionName) >= 1)
-                        $address[] = $ipdat->geoplugin_regionName;
-                    if (@strlen($ipdat->geoplugin_city) >= 1)
-                        $address[] = $ipdat->geoplugin_city;
-                    $output = implode(", ", array_reverse($address));
-                    break;
-                case "city":
-                    $output = @$ipdat->geoplugin_city;
-                    break;
-                case "state":
-                    $output = @$ipdat->geoplugin_regionName;
-                    break;
-                case "region":
-                    $output = @$ipdat->geoplugin_regionName;
-                    break;
-                case "country":
-                    $output = @$ipdat->geoplugin_countryName;
-                    break;
-                case "countrycode":
-                    $output = @$ipdat->geoplugin_countryCode;
-                    break;
-            }
-        }
-    }
-    return $output;
-}
-
-?>
